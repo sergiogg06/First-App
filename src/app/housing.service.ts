c3f1bc96f00ea99ce7db9d66497ec04614fb2077
sergiogg06 <sgalgri473@g.educaand.es>
2025-04-18 12:53:55 +0200

    12-forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -118,4 +118,9 @@ housingLocationList: HousingLocation[] = [
   }
   
   constructor() { }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
 }
